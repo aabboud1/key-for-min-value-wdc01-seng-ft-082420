9441d8cc9d419251964a3b818452cfc46e0521e2
immutable-hashmap-7791 <abboudahmed1@gmail.com>
2020-08-11 01:27:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,8 @@ def key_for_min_value(name_hash)
   min_key = nil
   name_hash.each do |min_value, min_key|
     if j == 0 
-      min_key = 0 
-      min_value = 0 
+      min_key = key 
+      min_value = value
+    
   end
 end
